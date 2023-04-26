0787139fc60f93f7561350c893c84c8d49a3e83b
Vladimir <eudvlad@gmail.com>
2023-04-26 18:29:29 +0300

    add regression_test add regression_test

---
+++ test_firefox.py
@@ -0,0 +1,31 @@
+import time
+
+import pytest
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.select import Select
+
+base_url = 'https://homepro.herokuapp.com/index.php'
+expected_url = 'https://homepro.herokuapp.com/order.php'
+expected_success_url = "https://homepro.herokuapp.com/orderconfirm.php"
+
+
+@pytest.mark.parametrize("name, lastname", [
+    ("Vladimir", "LastName2"),
+    ("Anton", "LastName2"),
+    ("Mia", "LastName2"),
+    ("Crabs", "LastName2"),
+    ("Alex", "LastName2")
+])
+@pytest.mark.regressiontest2
+def test_schedule_success(browser_firefox, name, lastname):
+    browser_firefox.get(base_url)
+    browser_firefox.find_element(By.LINK_TEXT, "Schedule an Appointment").click()
+    assert expected_url == browser_firefox.current_url
+    jobtype_dropdown = Select(browser_firefox.find_element(By.NAME, "job_type"))
+    jobtype_dropdown.select_by_visible_text("Decorating")
+    browser_firefox.find_element(By.NAME, "first_name").send_keys(name)
+    browser_firefox.find_element(By.NAME, "last_name").send_keys(lastname)
+    browser_firefox.find_element(By.NAME, "phone").send_keys("+7-951-289-75-12")
+    browser_firefox.find_element(By.NAME, "email").send_keys("eudvlad@gmail.com")
+    browser_firefox.find_element(By.XPATH, "//input[@value = 'Schedule My consultation']").click()
+    assert expected_success_url == browser_firefox.current_url
